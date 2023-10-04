6a9c925b730fe22b17fe1e3ec3a24457239daa57
Gihan Shirantha <shirantha1999@gmail.com>
2023-10-04 19:05:46 +0530

    Header section implementation

---
+++ app/Components/Header.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+
+const Header = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        isActive ? "bg-white py-4 shadow-md" : "bg-none py-6 "
+      } fixed w-full z-10 transition-all`}
+    >
+      <div>
+        <Link href={"/"}>
+          <h1 className="text-3xl font-bold text-center">Modern Walk</h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Header from './Components/Header'
 
 export default function Home() {
   return (
     <main>
+      <section className='flex flex-col pb-44'>
+        <Header/>
+      </section>
     
     </main>
   )
