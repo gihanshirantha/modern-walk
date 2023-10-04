95578d99126df5b9db8a7eb2f03660936892f809
Gihan Shirantha <shirantha1999@gmail.com>
2023-10-04 19:54:35 +0530

    fetch ane filter fashion products

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
-import Image from 'next/image'
-import Header from './Components/Header'
+"use client";
 
-export default function Home() {
+import Header from "./Components/Header";
+
+export default async function Home() {
+  //fetch products
+
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products: Product[] = await res.json();
+
+  //filter only fashion products
+  const filterProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+
+
   return (
     <main>
-      <section className='flex flex-col pb-44'>
-        <Header/>
+      <section className="flex flex-col pb-44">
+        <Header />
       </section>
-    
+      <section>
+        {filterProducts.map((product) => (
+          <div key={product.id}>
+            <h1>{product.title}</h1>
+          </div>
+        ))}
+      </section>
     </main>
-  )
+  );
 }
